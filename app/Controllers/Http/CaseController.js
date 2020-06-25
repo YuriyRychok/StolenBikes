7133f8f4a6834269fd7dfbf1723a9b199be80c17
yuriy.rychok <yuriy.rychok@gmail.com>
2020-06-25 15:11:01 +0300

    case controller updated

--- app/Controllers/Http/CaseController.js
+++ app/Controllers/Http/CaseController.js
@@ -4,6 +4,20 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Case = use('App/Models/Case')
+const { validate } = use('Validator')
+
+const rules = {
+  status: 'required|in:open, in_progress, resolved',
+  // details: '',
+  officer_id: 'required|integer',
+  bike_id: 'required|integer',
+  departament_id: 'required|integer',
+  created_at: 'date_format:YYYY-MM-DD HH:mm:ss',
+  updated_at: 'date_format:YYYY-MM-DD HH:mm:ss',
+  theft_date: 'date_format:YYYY-MM-DD HH:mm:ss',
+}
+
 /**
  * Resourceful controller for interacting with cases
  */
@@ -15,21 +29,70 @@ class CaseController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response}) {
+    const cases = await Case.all()
+
+    // converting to JSON array
+    const casesJSON = cases.toJSON()
+    return casesJSON
   }
 
   /**
-   * Render a form to be used for creating a new case.
-   * GET cases/create
+   * Show a list of active cases.
+   * GET cases/active
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
+  async getActive ({ request, response}) {
+    const cases = await Case
+      .query()
+      .where('status', '=', 'in_progress')
+      .fetch()
+
+    // converting to JSON array
+    const casesJSON = cases.toJSON()
+    return casesJSON
+  }
+
+  /**
+   * Show a list of closed cases.
+   * GET cases/closed
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async getClosed ({ request, response}) {
+    const cases = await Case
+      .query()
+      .where('status', '=', 'resolved')
+      .fetch()
+
+    // converting to JSON array
+    const casesJSON = cases.toJSON()
+    return casesJSON
+  }
+
+  /**
+   * Show a list of closed cases.
+   * GET cases/open
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async getOpen ({ request, response}) {
+    const cases = await Case
+      .query()
+      .where('status', '=', 'open')
+      .fetch()
+
+    // converting to JSON array
+    const casesJSON = cases.toJSON()
+    return casesJSON
   }
 
   /**
@@ -40,7 +103,23 @@ class CaseController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async create ({ request, response }) {
+    const body = request.post()
+
+    const validation = await validate(body, rules)
+
+    if (validation.fails()) {
+      return validation
+    }
+
+    var new_case = new Case()
+    new_case.status = body['status']
+    new_case.details = body['details']
+    new_case.officer_id = body['officer_id']
+    new_case.bike_id = body['bike_id']
+    new_case.departament_id = body['departament_id']
+
+    await new_case.save()
   }
 
   /**
@@ -50,32 +129,40 @@ class CaseController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing case.
-   * GET cases/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async getCaseById ({ params, request, response}) {
+    const { id } = params
+    return await Case.find(id)
   }
 
   /**
    * Update case details.
-   * PUT or PATCH cases/:id
+   * PUT cases/:id
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async edit ({ params, request, response }) {
+    const { id } = params
+    const body = request.all()
+
+    const validation = await validate(body, rules)
+
+    if (validation.fails()) {
+      return validation
+    }
+
+    await Case
+      .find(id)
+      .update({
+        // status: body['status'],
+        // details: body['details'],
+        // officer_id: body['officer_id'],
+        // bike_id: body['bike_id'],
+        // departament_id: body['departament_id'],
+      })
+
   }
 
   /**
@@ -86,7 +173,11 @@ class CaseController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async delete ({ params, request, response }) {
+    const { id } = params
+    const case1 = await Case.find(id)
+
+    await case1.delete()
   }
 }
 
